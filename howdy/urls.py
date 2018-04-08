cc9d0988824cd8d6d9cad263f52d9d21da4633c8
Akash <saiakash5@gmail.com>
2018-04-07 19:40:11 -0700

    added my resume and some modification need to be done to profile.html

--- howdy/urls.py
+++ howdy/urls.py
@@ -3,5 +3,5 @@ from howdy import views
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
-    url(r'^about/$', views.AboutPageView.as_view()), # Add this /about/ route
+    #url(r'^about/$', views.AboutPageView.as_view()), # Add this /about/ route
 ]
--- howdy/views.py
+++ howdy/views.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'index.html', context=None)
+        return render(request, 'profile.html', context=None)
     
     
 # Add this view
