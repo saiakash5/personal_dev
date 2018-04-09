86e12372533f69da21baddec3135b189af321835
Akash <saiakash5@gmail.com>
2018-04-09 13:38:24 -0700

    changed static files

--- helloapp/settings.py
+++ helloapp/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -118,10 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(STATIC_ROOT, 'staticfiles'),
-)
+STATIC_ROOT = "/Users/Akash/Documents/Django-dev/projects/hello/helloapp/howdy/static/"
 
 WKHTMLTOPDF_CMD = 'xvfb-run -a wkhtmltopdf'
 # database settings
--- howdy/views.py
+++ howdy/views.py
@@ -1,6 +1,10 @@
 #howdy/views.html
 from django.shortcuts import render
 from django.views.generic import TemplateView
+import os
+from django.conf import settings
+current_file_directory = os.path.dirname(__file__)
+file_path = os.path.join(current_file_directory,"../static/Akash_profile.pdf")
 
 # Create your views here.
 class HomePageView(TemplateView):
